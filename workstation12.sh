af4fa1747a63207be7a5e6a8dedf5f309a58c2f0
tanuja9346 <tanujach9346@gmail.com>
2023-11-23 13:55:50 +0530

    eksctl

--- workstation12.sh
+++ workstation12.sh
@@ -65,6 +65,11 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &>>$LOGFILE
 
 VALIDATE $? "kubectl installed"
 
+git clone https://github.com/ahmetb/kubectx /opt/kubectx &>>$LOGFILE
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens &>>$LOGFILE
+
+VALIDATE $? "kubens Installation"
+
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
